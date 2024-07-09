1f07192e271afc3a56fd8844c76ae0912cc1aaba
Pedro Cunha <idk.pedroc@gmail.com>
2024-07-09 13:11:33 -0300

    first model & training version

---
+++ final-challange-02/ml.py
@@ -0,0 +1,65 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from keras import layers
+from keras.src.models import Sequential
+
+
+TESTSET_DIR = "./testset/"
+TRAINSET_DIR = "./trainset/"
+BATCH_SIZE = 32
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    TRAINSET_DIR,
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    TESTSET_DIR,
+    validation_split=0.9,
+    subset="validation",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_dataset.class_names
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+num_classes = len(class_names)
+model = Sequential([
+    layers.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+model.summary()
+
+hostory = model.fit(train_dataset, validation_data=validation_dataset, epochs=10)
+
+model.save("geometry.h5")
