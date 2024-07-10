19b46c71a3bd027d5a9522c7df7f2fde51d4e973
Pedro Cunha <idk.pedroc@gmail.com>
2024-07-10 20:30:32 -0300

    machine learnign trainig with transfer learning

--- final-challange-m2/final_challange_m2/ml-tl.py
+++ final-challange-m2/final_challange_m2/ml-tl.py
@@ -4,7 +4,7 @@ warnings.filterwarnings('ignore')
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
-from keras.layers import  Dense
+from keras.layers import Dense
 import time
 
 init_time = time.time()
@@ -13,13 +13,13 @@ base_model_inceptionv3 = keras.applications.InceptionV3(weights='imagenet', incl
 x = base_model_inceptionv3.output
 x = keras.layers.GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
-predictions = Dense(1, activation='sigmoid')(x)
+predictions = Dense(9, activation='softmax')(x)
 model = Model(inputs=base_model_inceptionv3.input, outputs=predictions)
 
 for layer in base_model_inceptionv3.layers:
     layer.trainable = False
 
-model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.001), loss="binary_crossentropy", metrics=['accuracy',])
+model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.001), loss="categorical_crossentropy", metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale=1./255)
 validation_datagen = ImageDataGenerator(rescale=1./255)
@@ -33,17 +33,9 @@ train_generator = train_datagen.flow_from_directory(
 
 validation_generator = validation_datagen.flow_from_directory(
     "./testset/",
-    target_size=(150,150),
+    target_size=(150, 150),
     batch_size=32,
-    class_mode="binary"
-)
-
-history = model.fit(
-    train_generator, 
-    steps_per_epoch=train_generator.samples//train_generator.batch_size,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples//validation_generator.batch_size,
-    epochs=10
+    class_mode="categorical"
 )
 
 for layer in model.layers[:249]:
@@ -51,7 +43,7 @@ for layer in model.layers[:249]:
 for layer in model.layers[249:]:
     layer.trainable = True
 
-model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 history_fine_tuning = model.fit(
     train_generator,
@@ -64,4 +56,4 @@ history_fine_tuning = model.fit(
 model.save("geometry-tl.h5")
 
 end_time = time.time()
-print(f"O treinamento durou: {end_time-init_time} segundos")
+print(f"The training lasted: {end_time-init_time} seconds")
